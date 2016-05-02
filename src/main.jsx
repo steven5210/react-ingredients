e4c5b43f80e72d5b90118ed66a292dbbd60b084d
Steven5210 <shuynh5210@msn.com>
2016-05-02 16:03:24 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ListManager = require('./components/ListManager.jsx');
+
+ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'))
+ReactDOM.render(<ListManager title="Todo" />, document.getElementById('todo'))
+ReactDOM.render(<ListManager title="Christmas" headingColor="#b31217" btnColor="purple"/>, document.getElementById('christmas'))
